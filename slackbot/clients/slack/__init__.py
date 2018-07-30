91c9019ae90ec6b486fd515128855f9765e19f67
Mai Diep <diepmv@vccloud.vn>
2018-07-30 20:04:09 +0700

    add class variable slack_client

--- slackbot/clients/slack/__init__.py
+++ slackbot/clients/slack/__init__.py
@@ -2,9 +2,14 @@ from globals import slack_client
 
 
 class Member(object):
+	'''
+	The member slack object to get information about member like 
+	user_channel_id
+	'''
+	client = slack_client
+	
 	def __init__(self, member_id):
 		self.member_id = member_id
-		self.client = slack_client
 
 
 	def get_user_channel(self):
