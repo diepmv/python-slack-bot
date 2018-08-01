3c35a2922642598df4defde3230525afcd063315
Mai Diep <diepmv@vccloud.vn>
2018-08-02 06:13:03 +0700

    add config logging dict

--- slackbot/config.py
+++ slackbot/config.py
@@ -6,3 +6,39 @@ config.read('config.ini')
 
 
 rtmbot_config = yaml.load(open('rtmbot.conf', 'r'))
+
+_configDict = { 
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': { 
+        'standard': { 
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },'test': {
+        	'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        }
+    },
+    'handlers': { 
+        'default': { 
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },'test': {
+        	'level': 'DEBUG',
+        	'formatter': 'test'
+        }
+    },
+    'loggers': { 
+        '': { 
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        'slackbot': { 
+            'handlers': ['test'],
+            'level': 'WARN',
+            'propagate': False
+        },
+    } 
+}
+
+logging.config.dictConfig(_configDict)
